9113353e6b346575ab4827ed19603c2b6de8da79
KenzieQuery <william_query@comcast.com>
2021-07-16 15:04:55 -0600

    added EventData class and refactored Event Controller to use EventData

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.apache.coyote.Request;
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,13 +19,15 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-
-    private static List<Event> events = new ArrayList<>();
+// we are refactoring the Event Controller to use the EventData.java class
+// this starts with commenting out the line below. Then follow errors to finish editing the controller.
+//    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        // Part 2 refactoring for EventData.java class is adjusting the attribute to EventData.getAll
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -35,9 +38,9 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName,
-                                         @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
+        //Part 3 edit of controller to use EventData.java class is to change events to EventData.add
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
